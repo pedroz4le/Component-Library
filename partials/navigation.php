4ffc359f4839b63faa5fe13485c913e415799e54
Alejandra Pedroza <alejandra.pedroza.579@my.csun.edu>
2018-03-03 17:28:16 -0800

    styling Upgrades

--- partials/navigation.php
+++ partials/navigation.php
@@ -1,65 +1,65 @@
 <body>
 
   <div class="flex flex-wrap">
-    <div class="flex flex-col md:w-1/5 w-full md:h-screen">
-      <div class="bg-red-light h-full text-white overflow-scroll">
-        <h1 class="p-4">Component Library</h1>
+    <div class="flex flex-col md:w-1/5 md:h-screen mt-10 w-full">
+      <div class="h-full text-white overflow-scroll">
+        <h1 class="p-4"></h1>
         <nav>
           <ul class="font-bold list-reset m-8 px-6 text-lg">
             <li class="ml-4 mb-8">
-              <a class="no-underline text-grey-darkest hover:text-white" href="#">Alerts</a>
+              <a class="no-underline text-grey-darkest hover:text-grey-light" href="#">Alerts</a>
             </li>
 
             <li class="ml-4 mb-8">
-              <a class="no-underline text-grey-darkest hover:text-white" href="#">Badges</a>
+              <a class="no-underline text-grey-darkest hover:text-grey-light" href="#">Badges</a>
             </li>
 
             <li class="ml-4 mb-8">
-              <a class="no-underline text-grey-darkest hover:text-white" href="#">Breadcrumbs</a>
+              <a class="no-underline text-grey-darkest hover:text-grey-light" href="#">Breadcrumbs</a>
             </li>
               
             <li class="ml-4 mb-8">
-              <a class="no-underline text-grey-darkest hover:text-white" href="buttons.php">Buttons</a>
+              <a class="no-underline text-grey-darkest hover:text-grey-light" href="buttons.php">Buttons</a>
             </li>
 
             <li class="ml-4 mb-8">
-              <a class="no-underline text-grey-darkest hover:text-white" href="cards.php">Cards</a>
+              <a class="no-underline text-grey-darkest hover:text-grey-light" href="cards.php">Cards</a>
             </li>
 
             <li class="ml-4 mb-8">
-              <a class="no-underline text-grey-darkest hover:text-white" href="#">Footers</a>
+              <a class="no-underline text-grey-darkest hover:text-grey-light" href="#">Footers</a>
             </li>
 
             <li class="ml-4 mb-8">
-              <a class="no-underline text-grey-darkest hover:text-white" href="#">Forms</a>
+              <a class="no-underline text-grey-darkest hover:text-grey-light" href="#">Forms</a>
             </li>
 
             <li class="ml-4 mb-8">
-              <a class="no-underline text-grey-darkest hover:text-white" href="#">Global Navigation</a>
+              <a class="no-underline text-grey-darkest hover:text-grey-light" href="#">Global Navigation</a>
             </li>
 
             <li class="ml-4 mb-8">
-              <a class="no-underline text-grey-darkest hover:text-white" href="#">Hero Units</a>
+              <a class="no-underline text-grey-darkest hover:text-grey-light" href="#">Hero Units</a>
             </li>
 
             <li class="ml-4 mb-8">
-              <a class="no-underline text-grey-darkest hover:text-white" href="#">Lists</a>
+              <a class="no-underline text-grey-darkest hover:text-grey-light" href="#">Lists</a>
             </li>
 
             <li class="ml-4 mb-8">
-              <a class="no-underline text-grey-darkest hover:text-white" href="#">Media Objects</a>
+              <a class="no-underline text-grey-darkest hover:text-grey-light" href="#">Media Objects</a>
             </li>
 
             <li class="ml-4 mb-8">
-              <a class="no-underline text-grey-darkest hover:text-white" href="#">Navigations Lists</a>
+              <a class="no-underline text-grey-darkest hover:text-grey-light" href="#">Navigations Lists</a>
             </li>
 
             <li class="ml-4 mb-8">
-              <a class="no-underline text-grey-darkest hover:text-white" href="#">Pagination</a>
+              <a class="no-underline text-grey-darkest hover:text-grey-light" href="#">Pagination</a>
             </li>
 
             <li class="ml-4 mb-8">
-              <a class="no-underline text-grey-darkest hover:text-white" href="#">Tables</a>
+              <a class="no-underline text-grey-darkest hover:text-grey-light" href="#">Tables</a>
             </li>
 
           </ul>
--- partials/start.php
+++ partials/start.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html lang="en">
   <head>
-    <title>Tailwind Starter Template</title>
+    <title>Caff Source</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="./dist/styles.css">
@@ -13,6 +13,16 @@
   </head>
 
   <body>
+    <div class="fixed bg-white border-b w-screen">
+      <header class="ml-10 py-4">
+        <div class="flex items-center py-4 md:py-0 text-grey-light">
+          <img src="./images/logo.png" alt="caff source logo" class="w-16">
+          <h1 class="ml-4">Caff Source 
+            <span class="font-thin">Component Library</span>
+          </h1>
+        </div>
+      </header>
+    </div>
 
     <?php include 'partials/navigation.php'; ?>
-    <div class="flex flex-col md:h-screen overflow-y-scroll px-4 w-full md:w-4/5">
+    <div class="flex flex-col md:h-screen md:w-4/5 mt-10 overflow-y-scroll px-4 w-full">
