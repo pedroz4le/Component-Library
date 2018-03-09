1363925d38e3333d256e35dc9fb65633afe00315
Alejandra Pedroza <alejandra.pedroza.579@my.csun.edu>
2018-03-08 17:29:20 -0800

    Index Page

--- index.php
+++ index.php
@@ -1,5 +1,19 @@
 <?php include 'partials/start.php'; ?>
 
-  <h1>Welcome!</h1>
+<h1 class="pb-4">Welcome!</h1>
+
+<p class="leading-normal pb-8">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero perspiciatis quam corrupti quaerat in blanditiis mollitia quo unde impedit ullam incidunt quos id quia possimus, ipsum veniam nihil iure amet.
+
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero perspiciatis quam corrupti quaerat in blanditiis mollitia quo unde impedit ullam incidunt quos id quia possimus, ipsum veniam nihil iure amet.
+
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero perspiciatis quam corrupti quaerat in blanditiis mollitia quo unde impedit ullam incidunt quos id quia possimus, ipsum veniam nihil iure amet.
+
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero perspiciatis quam corrupti quaerat in blanditiis mollitia quo unde impedit ullam incidunt quos id quia possimus, ipsum veniam nihil iure amet.</p>
+
+<p class="leading-normal pb-8">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero perspiciatis quam corrupti quaerat in blanditiis mollitia quo unde impedit ullam incidunt quos id quia possimus, ipsum veniam nihil iure amet.
+
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero perspiciatis quam corrupti quaerat in blanditiis mollitia quo unde impedit ullam incidunt quos id quia possimus, ipsum veniam nihil iure amet.
+
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero perspiciatis quam corrupti quaerat in blanditiis mollitia quo unde impedit ullam incidunt quos id quia possimus, ipsum veniam nihil iure amet.</p>
 
 <?php include 'partials/end.php'; ?>
