3446c1bc2fa7b977a708512f099a51dce2e584bf
Alejandra Pedroza <alejandra.pedroza.579@my.csun.edu>
2018-03-11 22:47:15 -0700

    more table changes

--- tables.php
+++ tables.php
@@ -20,29 +20,33 @@ Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut dolorum fuga invent
 <div class="border border-grey-dark mb-8 mx-auto overflow-hidden rounded max-w-lg">
   <div class="border-b border-grey-dark p-8 text-center">
 
-    <div class="border border-grey-lighter flex flex-wrap mx-auto overflow-hidden rounded-lg max-w-lg">
-      <div class="bg-red border-r flex flex-col items-center text-white w-1/4">
-        <div class="p-6">ID</div>
-        <div class="p-6">Title</div>
-        <div class="p-6">Type</div>
+    <div class="mx-auto overflow-hidden rounded-lg max-w-lg">
+      <div class="flex flex-wrap">
+        <div class="bg-red border-b border-red-darker flex-1 p-6 text-center text-white">one</div>
+        <div class="bg-grey-light flex-1 p-6 text-center">two</div>
+        <div class="bg-grey-lighter flex-1 p-6 text-center">three</div>
+        <div class="bg-grey-light flex-1 p-6 text-center">four</div>
       </div>
 
-      <div class="flex flex-col items-center w-1/4">
-        <div class="p-6">1</div>
-        <div class="p-6">Nibh Quae</div>
-        <div class="p-6">Basic Page</div>
+      <div class="flex flex-wrap mx-auto max-w-lg">
+        <div class="bg-red border-b border-red-darker flex-1 p-6 text-center text-white">one</div>
+        <div class="bg-grey-lighter flex-1 p-6 text-center">two</div>
+        <div class="bg-grey-light flex-1 p-6 text-center">three</div>
+        <div class="bg-grey-lighter flex-1 p-6 text-center">four</div>
       </div>
 
-      <div class="bg-grey-lighter flex flex-col items-center w-1/4">
-        <div class="p-6">2</div>
-        <div class="p-6">Gravis Praesent</div>
-        <div class="p-6">Article</div>
+      <div class="flex flex-wrap mx-auto max-w-lg">
+        <div class="bg-red border-b border-red-darker flex-1 p-6 text-center text-white">one</div>
+        <div class="bg-grey-light flex-1 p-6 text-center">two</div>
+        <div class="bg-grey-lighter flex-1 p-6 text-center">three</div>
+        <div class="bg-grey-light flex-1 p-6 text-center">four</div>
       </div>
 
-      <div class="flex flex-col items-center w-1/4">
-        <div class="p-6">3</div>
-        <div class="p-6">Norstrud Voco</div>
-        <div class="p-6">Basic Page</div>
+      <div class="flex flex-wrap mx-auto max-w-lg">
+        <div class="bg-red border-b border-red-darker flex-1 p-6 text-center text-white">one</div>
+        <div class="bg-grey-lighter flex-1 p-6 text-center">two</div>
+        <div class="bg-grey-light flex-1 p-6 text-center">three</div>
+        <div class="bg-grey-lighter flex-1 p-6 text-center">four</div>
       </div>
     </div>
 
@@ -51,31 +55,7 @@ Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut dolorum fuga invent
   <div class="p-4 bg-grey-lightest">
     <pre class="language-html" style="margin: 0px; padding: 0px;">
       <code class="language-html text-sm">
-&lt;div class=&quot;border border-grey-lighter flex flex-wrap mx-auto overflow-hidden rounded-lg max-w-lg&quot;&gt;
-      &lt;div class=&quot;bg-red border-r flex flex-col items-center text-white w-1/4&quot;&gt;
-        &lt;div class=&quot;p-6&quot;&gt;ID&lt;/div&gt;
-        &lt;div class=&quot;p-6&quot;&gt;Title&lt;/div&gt;
-        &lt;div class=&quot;p-6&quot;&gt;Type&lt;/div&gt;
-      &lt;/div&gt;
 
-      &lt;div class=&quot;flex flex-col items-center w-1/4&quot;&gt;
-        &lt;div class=&quot;p-6&quot;&gt;1&lt;/div&gt;
-        &lt;div class=&quot;p-6&quot;&gt;Nibh Quae&lt;/div&gt;
-        &lt;div class=&quot;p-6&quot;&gt;Basic Page&lt;/div&gt;
-      &lt;/div&gt;
-
-      &lt;div class=&quot;bg-grey-lighter flex flex-col items-center w-1/4&quot;&gt;
-        &lt;div class=&quot;p-6&quot;&gt;2&lt;/div&gt;
-        &lt;div class=&quot;p-6&quot;&gt;Gravis Praesent&lt;/div&gt;
-        &lt;div class=&quot;p-6&quot;&gt;Article&lt;/div&gt;
-      &lt;/div&gt;
-
-      &lt;div class=&quot;flex flex-col items-center w-1/4&quot;&gt;
-        &lt;div class=&quot;p-6&quot;&gt;3&lt;/div&gt;
-        &lt;div class=&quot;p-6&quot;&gt;Norstrud Voco&lt;/div&gt;
-        &lt;div class=&quot;p-6&quot;&gt;Basic Page&lt;/div&gt;
-      &lt;/div&gt;
-    &lt;/div&gt;
       </code>
     </pre>
   </div>
@@ -90,33 +70,47 @@ Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut dolorum fuga invent
 
 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut dolorum fuga inventore, cumque, praesentium distinctio, fugit consectetur necessitatibus numquam accusamus rem beatae. Maxime similique voluptatem voluptatibus quaerat, voluptas harum, culpa?</p>
 
-<div class="mx-auto overflow-hidden rounded-lg max-w-lg">
-  <div class="flex flex-wrap">
-    <div class="bg-red border-r flex-1 p-6 text-center text-white">one</div>
-    <div class="bg-red border-r flex-1 p-6 text-center text-white">two</div>
-    <div class="bg-red border-r flex-1 p-6 text-center text-white">three</div>
-    <div class="bg-red flex-1 p-6 text-center text-white">four</div>
+<div class="border border-grey-dark mb-8 mx-auto overflow-hidden rounded max-w-lg">
+  <div class="border-b border-grey-dark p-8 text-center">
+
+    <div class="mx-auto overflow-hidden rounded-lg max-w-lg">
+      <div class="flex flex-wrap">
+        <div class="bg-red border-r border-red-darker flex-1 p-6 text-center text-white">one</div>
+        <div class="bg-red border-r border-red-darker flex-1 p-6 text-center text-white">two</div>
+        <div class="bg-red border-r border-red-darker flex-1 p-6 text-center text-white">three</div>
+        <div class="bg-red flex-1 p-6 text-center text-white">four</div>
+      </div>
+
+      <div class="flex flex-wrap mx-auto max-w-lg">
+        <div class="flex-1 bg-grey-light p-6 text-center">one</div>
+        <div class="flex-1 bg-grey-lighter p-6 text-center">two</div>
+        <div class="flex-1 bg-grey-light p-6 text-center">three</div>
+        <div class="flex-1 bg-grey-lighter p-6 text-center">four</div>
+      </div>
+
+      <div class="flex flex-wrap mx-auto max-w-lg">
+        <div class="flex-1 bg-grey-lighter p-6 text-center">one</div>
+        <div class="flex-1 bg-grey-light p-6 text-center">two</div>
+        <div class="flex-1 bg-grey-lighter p-6 text-center">three</div>
+        <div class="flex-1 bg-grey-light p-6 text-center">four</div>
+      </div>
+
+      <div class="flex flex-wrap mx-auto max-w-lg">
+        <div class="flex-1 bg-grey-light p-6 text-center">one</div>
+        <div class="flex-1 bg-grey-lighter p-6 text-center">two</div>
+        <div class="flex-1 bg-grey-light p-6 text-center">three</div>
+        <div class="flex-1 bg-grey-lighter p-6 text-center">four</div>
+      </div>
+    </div>
+
   </div>
 
-  <div class="flex flex-wrap mx-auto max-w-lg">
-    <div class="flex-1 bg-grey-light p-6 text-center">one</div>
-    <div class="flex-1 bg-grey-lighter p-6 text-center">two</div>
-    <div class="flex-1 bg-grey-light p-6 text-center">three</div>
-    <div class="flex-1 bg-grey-lighter p-6 text-center">four</div>
-  </div>
+  <div class="p-4 bg-grey-lightest">
+    <pre class="language-html" style="margin: 0px; padding: 0px;">
+      <code class="language-html text-sm">
 
-  <div class="flex flex-wrap mx-auto max-w-lg">
-    <div class="flex-1 bg-grey-lighter p-6 text-center">one</div>
-    <div class="flex-1 bg-grey-light p-6 text-center">two</div>
-    <div class="flex-1 bg-grey-lighter p-6 text-center">three</div>
-    <div class="flex-1 bg-grey-light p-6 text-center">four</div>
-  </div>
-
-  <div class="flex flex-wrap mx-auto max-w-lg">
-    <div class="flex-1 bg-grey-light p-6 text-center">one</div>
-    <div class="flex-1 bg-grey-lighter p-6 text-center">two</div>
-    <div class="flex-1 bg-grey-light p-6 text-center">three</div>
-    <div class="flex-1 bg-grey-lighter p-6 text-center">four</div>
+      </code>
+    </pre>
   </div>
 </div>
 
