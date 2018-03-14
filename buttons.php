c1d3a2ac7e7c71001e30e02ceb1c1dd1b9bde51b
Alejandra Pedroza <alejandra.pedroza.579@my.csun.edu>
2018-03-13 19:28:09 -0700

    index and buttons changes

--- buttons.php
+++ buttons.php
@@ -7,24 +7,26 @@ Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut dolorum fuga invent
 
 <div class="border-b w-full my-10"></div>
 
-<!-- ROUNDED BUTTON WITH ICON -->
+<!-- SOLID BUTTON -->
 
-<h2 class="font-thin pb-4">Pill Button</h2>
+<h2 class="font-thin pb-4">Solid Button</h2>
 <p class="leading-normal pb-8">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et unde non nam, consequatur expedita nulla fugiat tempora ex similique necessitatibus consequuntur mollitia eum soluta laudantium, repudiandae hic nobis minus laboriosam.</p>
 
 <div class="border border-grey-dark mb-8 mx-auto overflow-hidden rounded max-w-lg">
 	<div class="border-b border-grey-dark p-8 text-center">
-		<a class="bg-red font-bold inline-block hover:bg-red-lighter no-underline px-8 py-4 rounded-full shadow-sm text-lg text-white tracking-wide" href="#">
+
+		<a class="bg-brand-gradient font-bold inline-block hover:bg-brand-gradient hover:bg-red-lighter no-underline px-8 py-4 rounded-full shadow-sm text-lg text-white tracking-wide" href="#">
 			<div class="flex items-center">
 				Button<i class="fas fa-lg fa-angle-right ml-6"></i>
 			</div>
 		</a>
+
 	</div>
 
-	<div class="p-4 bg-grey-lightest">
+	<div class="px-4 bg-grey-lightest">
 		<pre class="language-html" style="margin: 0px; padding: 0px;">
 			<code class="language-html text-sm">
-&lt;a class=&quot;bg-red font-bold inline-block hover:bg-red-lighter no-underline px-8 py-4 rounded-full shadow-sm text-lg text-white tracking-wide&quot; href=&quot;#&quot;&gt;
+&lt;a class=&quot;bg-brand-gradient font-bold inline-block hover:bg-brand-gradient hover:bg-red-lighter no-underline px-8 py-4 rounded-full shadow-sm text-lg text-white tracking-wide&quot; href=&quot;#&quot;&gt;
 	&lt;div class=&quot;flex items-center&quot;&gt;
 		Button&lt;i class=&quot;fas fa-lg fa-angle-right ml-6&quot;&gt;&lt;/i&gt;
 	&lt;/div&gt;
@@ -36,20 +38,70 @@ Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut dolorum fuga invent
 
 <div class="border-b w-full my-10"></div>
 
-<!-- SQUARED BUTTON -->
+<!-- GHOST BUTTON -->
 
-<h2 class="font-thin pb-4">Squared Button</h2>
+<h2 class="font-thin pb-4">Ghost Button</h2>
 <p class="leading-normal pb-8">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et unde non nam, consequatur expedita nulla fugiat tempora ex similique necessitatibus consequuntur mollitia eum soluta laudantium, repudiandae hic nobis minus laboriosam.</p>
 
-<a class="bg-theme-redish font-bold inline-block hover:bg-theme-redish-light no-underline px-8 py-4 rounded-sm shadow-sm text-lg text-white tracking-wide" href="#">
-	<div class="flex items-center">Button
-	</div>
-</a>
+<div class="border border-grey-dark mb-8 mx-auto overflow-hidden rounded max-w-lg">
+	<div class="border-b border-grey-dark p-8 text-center">
 
-<pre><code class="html overflow-auto my-8">&lt;a class=&quot;bg-theme-redish font-bold inline-block hover:bg-theme-redish-light no-underline px-8 py-4 rounded-sm shadow-sm text-lg text-white tracking-wide&quot; href=&quot;#&quot;&gt;
-	&lt;div class=&quot;flex items-center&quot;&gt;Button
+		<a class="border border-red font-bold hover:bg-red hover:text-white inline-block no-underline px-8 py-4 rounded-full text-lg text-red tracking-wide" href="#">
+			<div class="flex items-center">
+				Button<i class="fas fa-lg fa-angle-right ml-6"></i>
+			</div>
+		</a>
+
+	</div>
+
+	<div class="px-4 bg-grey-lightest">
+		<pre class="language-html" style="margin: 0px; padding: 0px;">
+			<code class="language-html text-sm">
+&lt;a class=&quot;border border-red font-bold hover:bg-red hover:text-white inline-block no-underline px-8 py-4 rounded-full text-lg text-red tracking-wide&quot; href=&quot;#&quot;&gt;
+	&lt;div class=&quot;flex items-center&quot;&gt;
+		Button&lt;i class=&quot;fas fa-lg fa-angle-right ml-6&quot;&gt;&lt;/i&gt;
 	&lt;/div&gt;
 &lt;/a&gt;
-</code></pre>
+			</code>
+		</pre>
+	</div>
+</div>
+
+<div class="border-b w-full my-10"></div>
+
+<!-- MULTIPLE OPTION BUTTON -->
+
+<h2 class="font-thin pb-4">Multiple Option Button</h2>
+<p class="leading-normal pb-8">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et unde non nam, consequatur expedita nulla fugiat tempora ex similique necessitatibus consequuntur mollitia eum soluta laudantium, repudiandae hic nobis minus laboriosam.</p>
+
+<div class="border border-grey-dark mb-8 mx-auto overflow-hidden rounded max-w-lg">
+	<div class="border-b border-grey-dark p-8 text-center">
+
+		<div class="inline-flex">
+		  <a class="bg-purple border border-purple-dark hover:bg-purple-light text-lg text-white font-bold py-4 px-8 rounded-l-full tracking-wide">
+		    <i class="fas fa-angle-left mr-6"></i> Prev
+		  </a>
+		  <a class="bg-purple border border-purple-dark hover:bg-purple-light text-lg text-white font-bold py-4 px-8 rounded-r-full tracking-wide">
+		    Next<i class="fas fa-lg fa-angle-right ml-6"></i>
+		  </a>
+		</div>
+
+	</div>
+
+	<div class="px-4 bg-grey-lightest">
+		<pre class="language-html" style="margin: 0px; padding: 0px;">
+			<code class="language-html text-sm">
+&lt;div class=&quot;inline-flex&quot;&gt;
+  &lt;a class=&quot;bg-purple border border-purple-dark hover:bg-purple-light text-lg text-white font-bold py-4 px-8 rounded-l-full tracking-wide&quot;&gt;
+    &lt;i class=&quot;fas fa-angle-left mr-6&quot;&gt;&lt;/i&gt; Prev
+  &lt;/a&gt;
+  &lt;a class=&quot;bg-purple border border-purple-dark hover:bg-purple-light text-lg text-white font-bold py-4 px-8 rounded-r-full tracking-wide&quot;&gt;
+    Next&lt;i class=&quot;fas fa-lg fa-angle-right ml-6&quot;&gt;&lt;/i&gt;
+  &lt;/a&gt;
+&lt;/div&gt;
+			</code>
+		</pre>
+	</div>
+</div>
 
 <?php include 'partials/end.php'; ?>
--- index.php
+++ index.php
@@ -14,23 +14,23 @@
     <script src="https://cdn.jsdelivr.net/npm/vue@2.5.13/dist/vue.js"></script>
 </head>
 	<body class="">
-		<div class="bg-center bg-brand-gradient border-t-8 border-grey-light flex items-center justify-center min-h-screen ">
+		<div class="bg-center bg-my-pattern border-t-8 border-grey-light flex items-center justify-center min-h-screen ">
 			<div>
 				<div>
 					<div class="bg-white border border-8 border-grey h-32 mx-auto m-8 overflow-auto rounded-full shadow-lg w-32">
 						<img src="./images/logoSquare.png" alt="logo">
 					</div>
 
-					<h1 class="mb-4 text-center text-white text-3xl">Caff Source</h1>
+					<h1 class="mb-4 text-center text-black text-3xl">Caff Source</h1>
 
-					<h2 class="font-thin mb-4 text-center text-white text-4xl">Component Library</h2>
+					<h2 class="font-thin mb-4 text-center text-black text-4xl">Component Library</h2>
 
 					<div class="">
-						<a class="bg-white font-bold hover:bg-grey-light inline-block m-4 no-underline px-8 py-4 rounded-full shadow-sm text-lg text-home" href="aboutCaffSource.php">
+						<a class="bg-brand-gradient hover:bg-brand-gradient hover:bg-red-light font-bold inline-block m-4 no-underline px-8 py-4 rounded-full shadow-sm text-lg text-white" href="aboutCaffSource.php">
 							Learn More
 						</a>
 
-						<a class="border-2 border-white font-bold hover:bg-grey-light hover:text-home hover:border-home inline-block m-4 no-underline px-8 py-4 rounded-full shadow-sm text-lg text-white" href="https://github.com/pedroz4le/Component-Library">
+						<a class="border-2 border-red font-bold hover:bg-red hover:text-white inline-block m-4 no-underline px-8 py-4 rounded-full shadow-sm text-lg text-red" href="https://github.com/pedroz4le/Component-Library">
 							GitHub
 						</a>
 					</div>
