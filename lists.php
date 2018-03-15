5a81ad4db5c50cf597c22f438d254b5128e71a78
Alejandra Pedroza <alejandra.pedroza.579@my.csun.edu>
2018-03-15 13:44:07 -0700

    media objects and Lists

--- lists.php
+++ lists.php
@@ -8,23 +8,84 @@ Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut dolorum fuga invent
 
 <div class="border-b w-full my-10"></div>
 
-<!-- NUMBERED LIST -->
+<!-- ARTICLE LIST -->
 
-<h2 class="font-thin pb-4">Numbered List</h2>
+<h2 class="font-thin pb-4">Article List</h2>
 <p class="leading-normal pb-8">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et unde non nam, consequatur expedita nulla fugiat tempora ex similique necessitatibus consequuntur mollitia eum soluta laudantium, repudiandae hic nobis minus laboriosam.</p>
 
-<ol>
-  <li>Coffee</li>
-  <li>Tea</li>
-  <li>Coca Cola</li>
-</ol>
+<div class="border border-grey-dark mb-8 mx-auto overflow-scroll rounded max-w-xl">
+  <div class="border-b border-grey-dark overflow-scroll p-8">
 
-<pre><code class="html overflow-auto my-8">&lt;ol&gt;
-  &lt;li&gt;Coffee&lt;/li&gt;
-  &lt;li&gt;Tea&lt;/li&gt;
-  &lt;li&gt;Coca Cola&lt;/li&gt;
-&lt;/ol&gt;
-</code></pre>
+    <div class="md:flex flex-wrap lg:flex-no-wrap h-96 p-8 max-w-xl w-full">
+      <div class="md:flex w-full md:1/2">
+        <div class="bg-red md:flex flex-col w-full md:w-1/3">
+          <div class="p-4">
+            <p>Jan. 16</p>
+            <p>11:09 a.m</p>
+            <h2>Today</h2>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure eaque repellat officiis recusandae sapiente quae
+          </div>
+        </div>
+
+        <div class="bg-green md:flex flex-col w-full md:w-1/3">
+          <div class="p-4">
+            <p>Jan. 16</p>
+            <p>11:09 a.m</p>
+            <h2>Today</h2>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure eaque repellat officiis recusandae sapiente quae
+          </div>
+        </div>
+
+        <div class="bg-blue md:flex flex-col w-full md:w-1/3">
+          <div class="p-4">
+            <p>Jan. 16</p>
+            <p>11:09 a.m</p>
+            <h2>Today</h2>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure eaque repellat officiis recusandae sapiente quae
+          </div>
+        </div>
+      </div>
+
+      <div class="md:flex w-full md:1/2">
+        <div class="bg-yellow md:flex flex-col w-full md:w-1/3">
+          <div class="p-4">
+            <p>Jan. 16</p>
+            <p>11:09 a.m</p>
+            <h2>Today</h2>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure eaque repellat officiis recusandae sapiente quae
+          </div>
+        </div>
+
+        <div class="bg-purple md:flex flex-col w-full md:w-1/3">
+          <div class="p-4">
+            <p>Jan. 16</p>
+            <p>11:09 a.m</p>
+            <h2>Today</h2>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure eaque repellat officiis recusandae sapiente quae
+          </div>
+        </div>
+
+        <div class="bg-teal md:flex flex-col w-full md:w-1/3">
+          <div class="p-4">
+            <p>Jan. 16</p>
+            <p>11:09 a.m</p>
+            <h2>Today</h2>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure eaque repellat officiis recusandae sapiente quae
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+
+  <div class="px-4 bg-grey-lightest">
+    <pre class="language-html" style="margin: 0px; padding: 0px;">
+      <code class="language-html text-sm">
+
+      </code>
+    </pre>
+  </div>
+</div>
 
 <div class="border-b w-full my-10"></div>
 
@@ -33,17 +94,6 @@ Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut dolorum fuga invent
 <h2 class="font-thin pb-4">Bullet List</h2>
 <p class="leading-normal pb-8">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et unde non nam, consequatur expedita nulla fugiat tempora ex similique necessitatibus consequuntur mollitia eum soluta laudantium, repudiandae hic nobis minus laboriosam.</p>
 
-<ul>
-  <li>Coffee</li>
-  <li>Tea</li>
-  <li>Coca Cola</li>
-</ul>
 
-<pre><code class="html overflow-auto my-8">&lt;ul&gt;
-  &lt;li&gt;Coffee&lt;/li&gt;
-  &lt;li&gt;Tea&lt;/li&gt;
-  &lt;li&gt;Coca Cola&lt;/li&gt;
-&lt;/ul&gt;
-</code></pre>
 
 <?php include'partials/end.php'; ?>
--- mediaObjects.php
+++ mediaObjects.php
@@ -14,32 +14,54 @@
 <h2 class="font-thin pb-4">User Comment</h2>
 <p class="leading-normal pb-8">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et unde non nam, consequatur expedita nulla fugiat tempora ex similique necessitatibus consequuntur mollitia eum soluta laudantium, repudiandae hic nobis minus laboriosam.</p>
 
-<div class="border mx-auto rounded-lg w-3/4">
-  <article class="flex flex-wrap p-8">
-    <div class="w-1/4 px-2">
-      <img src="http://via.placeholder.com/135x135" alt="Name" class="rounded-full">
-    </div>
-    <div class="w-3/4 px-2">
-      <header class="border-b mb-6">
-        <h1 class="text-2xl pb-2">
-          Person Name
-        </h1>
-        <p class="font-thin pb-2">March 1, 2018 at 8:21 p.m.</p>
-      </header>
-      <p class="leading-normal">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates accusamus quae quibusdam possimus, officia, omnis sint pariatur dolorem porro illum molestias praesentium, reiciendis quaerat culpa accusantium assumenda distinctio facere. Quis!</p>
-    </div>
-  </article>
-</div>
+<div class="border border-grey-dark mb-8 mx-auto overflow-hidden rounded max-w-lg">
+  <div class="border-b border-grey-dark p-8">
 
-<pre><code class="html overflow-auto my-8">&lt;div class=&quot;border mx-auto rounded-lg w-3/4&quot;&gt;
-  &lt;article class=&quot;flex flex-wrap p-8&quot;&gt;
-    &lt;div class=&quot;w-1/4 px-2&quot;&gt;
-      &lt;img src=&quot;http://via.placeholder.com/135x135&quot; alt=&quot;Name&quot; class=&quot;rounded-full&quot;&gt;
+    <div class="border mx-auto rounded-lg max-w-lg">
+      <article class="md:flex flex-wrap p-8 shadow-sm">
+        <div class="md:flex flex-col mx-auto pb-6 px-2 w-full md:w-1/4">
+          <img src="../images/profilePicOne.png" alt="Name" class="border border-grey h-32 mb-6 rounded-full shadow-md w-32">
+
+          <a class="bg-brand-gradient font-bold inline-block hover:bg-brand-gradient hover:bg-red-lighter no-underline px-4 py-2 rounded-full shadow-sm text-lg text-white tracking-wide w-32 " href="#">
+            <div class="flex items-center justify-center">
+              Follow
+            </div>
+          </a>
+        </div>
+
+        <div class="md:flex flex-col px-2 w-full md:w-3/4">
+          <header class="border-b border-grey mb-6">
+            <h1 class="text-2xl pb-2">
+              Jane Doe
+            </h1>
+            <p class="font-thin pb-2">March 1, 2018 at 8:21 p.m.</p>
+          </header>
+          <p class="leading-normal">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates accusamus quae quibusdam possimus, officia, omnis sint pariatur dolorem porro illum molestias praesentium, reiciendis quaerat culpa accusantium assumenda distinctio facere. Quis!</p>
+        </div>
+      </article>
+    </div>
+
+  </div>
+
+  <div class="px-4 bg-grey-lightest">
+    <pre class="language-html" style="margin: 0px; padding: 0px;">
+      <code class="language-html text-sm">
+&lt;div class=&quot;border mx-auto rounded-lg max-w-lg&quot;&gt;
+  &lt;article class=&quot;md:flex flex-wrap p-8 shadow-sm&quot;&gt;
+    &lt;div class=&quot;md:flex flex-col mx-auto pb-6 px-2 w-full md:w-1/4&quot;&gt;
+      &lt;img src=&quot;../images/profilePicOne.png&quot; alt=&quot;Name&quot; class=&quot;border border-grey h-32 mb-6 rounded-full shadow-md w-32&quot;&gt;
+
+      &lt;a class=&quot;bg-brand-gradient font-bold inline-block hover:bg-brand-gradient hover:bg-red-lighter no-underline px-4 py-2 rounded-full shadow-sm text-lg text-white tracking-wide w-32 &quot; href=&quot;#&quot;&gt;
+        &lt;div class=&quot;flex items-center justify-center&quot;&gt;
+          Follow
+        &lt;/div&gt;
+      &lt;/a&gt;
     &lt;/div&gt;
-    &lt;div class=&quot;w-3/4 px-2&quot;&gt;
-      &lt;header class=&quot;border-b mb-6&quot;&gt;
+
+    &lt;div class=&quot;md:flex flex-col px-2 w-full md:w-3/4&quot;&gt;
+      &lt;header class=&quot;border-b border-grey mb-6&quot;&gt;
         &lt;h1 class=&quot;text-2xl pb-2&quot;&gt;
-          Person Name
+          Jane Doe
         &lt;/h1&gt;
         &lt;p class=&quot;font-thin pb-2&quot;&gt;March 1, 2018 at 8:21 p.m.&lt;/p&gt;
       &lt;/header&gt;
@@ -47,7 +69,12 @@
     &lt;/div&gt;
   &lt;/article&gt;
 &lt;/div&gt;
-</code></pre>
+      </code>
+    </pre>
+  </div>
+</div>
+
+
 
 <div class="border-b w-full my-10"></div>
 
